a8d9ac9caea9d581a676c93cb87c0cc7f8abf86f
LawrieDrew <laurenn.drew@gmail.com>
2021-04-12 17:36:32 -0400

    added manager test script, not yet tested

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -1,54 +1,54 @@
-const Intern = require('../lib/Intern');
+const Manager = require('../lib/Manager');
 
-describe('Intern', () => {
+describe('Manager', () => {
  //all tests will be here
     describe('Initialization', () => {
 
-        it('object of Intern class', () => {
+        it('object of Manager class', () => {
 
-            const intern = new Intern();
+            const manager = new Manager();
 
-            expect(intern instanceof Intern).tobeTruthy()
+            expect(manager instanceof Manager).tobeTruthy()
         } );
 
         it ('sets name property based on contructor argument', () => {
             const name = "John";
-            const Intern = new Intern(name);
+            const manager = new Manager(name);
 
-            expect(intern.name).toBe(name);
+            expect(manager.name).toBe(name);
         });
 
         it ('it sets id property based on contructor argument', () => {
             const id = 1;
-            const intern = new Intern("", id);
+            const manager = new Manager("", id);
 
-            expect(intern.id).toBe(id);
+            expect(manager.id).toBe(id);
         });
 
 
         it ('it sets email property based on contructor argument', () => {
             const email = 'test@gmail.com';
-            const intern = new intern("",0, email);
+            const manager = new Manager("",0, email);
 
-            expect(intern.email).toBe(email);
+            expect(manager.email).toBe(email);
         });
     });
 
         describe('getName', () => {
             it('returns name when getName() is used', () => {
                     const name = "John";
-                    const intern = new Intern(name);
+                    const manager = new Manager(name);
 
-                    expect(intern.getName()).toBe(name);
+                    expect(manager.getName()).toBe(name);
             });
         });
 
         describe('getId', () => {
             it('returns id when getId() is used', () => {        
                 const id = 1;
-                const intern = new Intern('', id);
+                const manager = new Manager('', id);
 
-                expect(intern.getId()).toBe(id);
+                expect(manager.getId()).toBe(id);
 
         });
     });
@@ -56,28 +56,28 @@ describe('Intern', () => {
     describe('getEmail', () => {
             it('returns email when getEmail() is used', () => {
                 const email = 'test@gmail.com';
-                const intern = new Intern('', 0, email);
+                const manager = new Manager('', 0, email);
 
-                expect(intern.getEmail()).toBe(email);
+                expect(manager.getEmail()).toBe(email);
             });
         });
 
         describe('getRole', () => {
             it('it sets role property based on constructor argument', () => {
                 const  role = 'role';
-                const intern = new Intern("", role);
+                const manager = Manager("", role);
     
-                expect(intern.role).toBe(role);
+                expect(manager.role).toBe(role);
     
             });
         });
 
-        describe('getSchool', () => {
-            it('returns Interns school name when getSchool() is called', () => {
-                const school = 'Not A Thing University';
-                const intern = new Intern("", 1, 'test@gmail.com', school);
+        describe('getOfficeNumber', () => {
+            it('returns managers office number when getOfficeNumber() is called', () => {
+                const officenumber = 'Not A Thing University';
+                const manager = new Manager("", 1, 'test@gmail.com', officenumber);
 
-                expect(intern.getSchool()).toBe(school);
+                expect(manager.getOfficeNumber()).toBe(officenumber);
 
             });
 
