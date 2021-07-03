44a4ee3bda9a4acb55325701af7d0f01d07644e8
LawrieDrew <laurenn.drew@gmail.com>
2021-07-03 12:56:16 -0400

    fixed some bugs, added readme markdown

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
+# Table of Contents:
+* [Title](#Title)
+* [Description](#Description)
+* [Usage](#Usage)
+* [Live Demo](#Demo)
+* [Contribution](#Contribution)
+* [Tests](#Tests)
+* [License](#License)
+* [Github](#Github)
+* [Email](#Email)
+
+# Title:
 # Team-Profile-Generator
+
+# Description:
+This is a README markdown file generator created for my Columbia Engineering Full Stack Web Development Bootcamp.
+
+# Usage:
+https://github.com/LawrieDrew/
+
+# Demo:
+https://drive.google.com/file/d/1Vb5Jv1PJ67FELEDuSXdDESxEiKNwG4tj/view
+
+# Contribution:
+My project is in a public repository on Github which is an open source platform. You may even be on it now. This project can be forked and all contribution is welcome!
+
+# Tests:
+The initial successful test run of this generator is saved in the project repository in a file entitled ReadMe Demos.
+
+# License:
+
+MIT License
+
+Copyright (c) 2021 Lauren Drew
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+# Github:
+Please visit my https://github.com/LawrieDrew/ to visit my work.
+
+# Email:
+My Email is laurenn.drew@gmail.com. Feel free to reach me here.
--- index.js
+++ index.js
@@ -12,6 +12,148 @@ const outputPath = path.join(OUTPUT_DIR, "teamprofile.html");
 let team = [];
 
 function init() {
-
+    inquirer.prompt({
+        type: 'list',
+        message: 'What type of employee would you like to add?',
+        name: 'switch',
+        choices: ['Manager', 'Engineer', 'Intern', 'Exit']
+    }) .then(answer =>{
+        if(answer.switch === 'Manager' ){
+            askManager()
+        } else if(answer.switch === 'Engineer'){
+            askEngineer()
+        } else if(answer.switch === 'Intern'){
+            askIntern()
+        } else{
+            buildTeam()
+        }
+    })
 
 }
+const managerQues = [
+    {
+        type: 'input',
+        message: 'What is the managers name',
+        name: 'manName',
+    },
+    {
+        type: 'input',
+        message: 'What is the managers email',
+        name: 'manEmail',
+    },
+    {
+        type: 'input',
+        message: 'What is the managers ID',
+        name: 'manID',
+    },
+    {
+        type: 'input',
+        message: 'What is the managers office number',
+        name: 'manOffNum',
+    }
+];
+const engineerQues = [
+    {
+        type: 'input',
+        message: 'What is the engineer name',
+        name: 'engName',
+    },
+    {
+        type: 'input',
+        message: 'What is the engineer email',
+        name: 'engEmail',
+    },
+    {
+        type: 'input',
+        message: 'What is the engineer ID',
+        name: 'engID',
+    },
+    {
+        type: 'input',
+        message: 'What is the engineer Github',
+        name: 'engGitHub',
+    }
+];
+const internQues = [
+    {
+        type: 'input',
+        message: 'What is the intern name',
+        name: 'intName',
+    },
+    {
+        type: 'input',
+        message: 'What is the intern email',
+        name: 'intEmail',
+    },
+    {
+        type: 'input',
+        message: 'What is the intern ID',
+        name: 'intID',
+    },
+    {
+        type: 'input',
+        message: 'What is the interns school',
+        name: 'intSchool',
+    }
+];
+function askManager(){
+    inquirer.prompt(managerQues).then(answers =>{
+         const manager = new Manager(answers.manName, answers.manID, answers.manEmail, answers.manOffNum);
+        team.push(manager)
+        console.log(team);
+        init();
+    })
+}
+
+function askEngineer(){
+    inquirer.prompt(engineerQues).then(answers =>{
+         const engineer = new Engineer(answers.engName, answers.engID, answers.engEmail, answers.engGitHub);
+        team.push(engineer)
+        console.log(team);
+        init();
+    })
+}
+function askIntern(){
+    inquirer.prompt(internQues).then(answers =>{
+         const intern = new Intern (answers.intName, answers.intID, answers.intEmail, answers.intSchool);
+        team.push(intern)
+        console.log(team);
+        init();
+    })
+}
+//do this for the other guys 
+//loop over these dudes with get role method to fig out what ee they are (askRob)
+//dont leave dangling problems
+
+function buildTeam(){
+    if(!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    fs.writeFileSync(outputPath, renderHTML(team), "utf8")    
+}
+
+function renderHTML(team){
+    console.log(team);
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+        
+        <title>Team-Profile-Generator</title>
+    </head>
+    <body>
+        <div class="jumbotron jumbotron-fluid .bg-primary text-center">
+            <div class="container">
+              <h1 class="display-4">Corporate Office Roster</h1>
+            </div>
+          </div>
+         
+            
+    </body>
+    </html>`
+    
+}
+init()
